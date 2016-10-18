cddc41903784acd560dfae65b5782a8ea8383298
kÆT <kaet@users.noreply.github.com>
2016-10-18 10:20:32 +0200

    Changed atom tag order.

--- atom.js
+++ atom.js
@@ -17,11 +17,11 @@ class AtomFeed {
           root.node(node, date.toISOString()).parent()
       , link: (root, rel, href) =>
           root.node('link').attr({ rel: rel.slice(5), href }).parent()
-      , person: (root, node, person) => {
-          root = root.node(node)
-          root.node('name', person.name).parent()
-          if (person.email) root.node('email', person.email).parent()
-          if (person.uri) root.node('uri', person.uri).parent()
+      , person: (root, type, person) => {
+          node = root.node(type)
+          node.node('name', person.name).parent()
+          if (person.email) node.node('email', person.email).parent()
+          if (person.uri) node.node('uri', person.uri).parent()
         }
       }
     this.feed(opts)
@@ -30,43 +30,44 @@ class AtomFeed {
   feed (opts) {
     this.root = this.document.node('feed')
       .attr({ xmlns: 'http://www.w3.org/2005/Atom' })
-    this.build(this.root
-      , { id: ['common', opts.id || new Error('Feed ID required.')]
-        , title: ['text', opts.title || new Error('Feed title required.')]
-        , subtitle: ['text', opts.subtitle]
-        , updated: ['date', opts.updated || new Date]
-        , author: ['person', opts.author || new Error('Feed author required.')]
-        , contributor: ['person', opts.contributor]
-        , link_self: ['link', opts.link_self]
-        , link_alternate: ['link', opts.link_alternate]
-        , link_related: ['link', opts.link_related]
-        , logo: ['common', opts.logo]
-        , icon: ['common', opts.icon]
-        , rights: ['text', opts.rights]
-        , generator: ['common', opts.generator]
-      })
+    let struct =
+      { id: ['common', opts.id || new Error('Feed ID required.')]
+      , title: ['text', opts.title || new Error('Feed title required.')]
+      , subtitle: ['text', opts.subtitle]
+      , link_self: ['link', opts.link_self]
+      , link_alternate: ['link', opts.link_alternate]
+      , link_related: ['link', opts.link_related]
+      , author: ['person', opts.author || new Error('Feed author required.')]
+      , contributor: ['person', opts.contributor]
+      , logo: ['common', opts.logo]
+      , icon: ['common', opts.icon]
+      , rights: ['text', opts.rights]
+      , generator: ['common', opts.generator]
+      , updated: ['date', opts.updated || new Date]
+      }
+    this.build(this.root, struct)
   }
 
   entry (item) {
     let entry = this.root.node('entry')
-    this.build(entry
-      , { id: ['common', item.id || new Error('Entry ID required.')]
-        , title: ['html', item.title || new Error('Entry title required.')]
-        , updated: ['date', item.updated || new Date]
-        , published: ['date', item.published]
-        , author: ['person', item.author]
-        , contributor: ['person', item.contributor]
-        , summary: ['text', item.summary]
-        , content: ['html', item.content]
-        , link_alternate: ['link', item.link_alternate]
-        , link_enclosure: ['link', item.link_enclosure]
-      })
+    let struct =
+      { id: ['common', item.id || new Error('Entry ID required.')]
+      , title: ['html', item.title || new Error('Entry title required.')]
+      , link_alternate: ['link', item.link_alternate]
+      , link_enclosure: ['link', item.link_enclosure]
+      , summary: ['text', item.summary]
+      , content: ['html', item.content]
+      , author: ['person', item.author]
+      , contributor: ['person', item.contributor]
+      , updated: ['date', item.updated || new Date]
+      , published: ['date', item.published]
+      }
+    this.build(entry, struct)
   }
 
   build (root, data) {
     for (let field in data) {
-      let value = data[field][1]
-      let construct = data[field][0]
+      let [value, construct] = data[field]
 
       if (!value) continue
       if (value instanceof Error) throw value
