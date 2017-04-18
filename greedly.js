f492b5db513aceb4745de8a7658b76fdd870047e
kÆT <kaet@users.noreply.github.com>
2017-04-18 15:30:36 +0200

    Control flow style update.

--- greedly.js
+++ greedly.js
@@ -7,45 +7,38 @@ const later = require('later')
 
 class Feed {
 
-  constructor (opts) {
+  constructor(opts) {
     this.opts = opts
     this.cache = []
 
-    let structure = {}
-    for (let field in opts.fields) {
-      structure[field] = opts.fields[field].select
-    }
+    const struct = {}
+    Object.entries(opts.fields).map([key, value] => struct[key] = value.select)
 
-    this.request = new osmosis(opts.feed.link_alternate
-      , opts.feed.request)
-        .find(opts.feed.root)
-        .set(structure)
-        .data(this._data.bind(this))
-        .done(this._done.bind(this))
+    this.request = new osmosis(opts.feed.link_alternate, opts.feed.request)
+      .find(opts.feed.root)
+      .set(struct)
+      .data(this._data.bind(this))
+      .done(this._done.bind(this))
   }
 
-  fetch (callback) {
+  fetch(callback) {
     this.items = []
     this.callback = callback
     this.request.run()
   }
 
-  _data (obj) {
-    let item = { updated: new Date }
-
-    let concat = ''
-    for (let field in obj) {
-      concat += obj[field]
-    }
-    item.md5 = crypto.createHash('md5')
-      .update(concat)
-      .digest('hex')
+  _data(obj) {
+    const concat = Object.values(obj).join('')
+    const item =
+        { updated: new Date
+        , md5: crypto.createHash('md5').update(concat).digest('hex')
+        }
 
     if (this.cache.includes(item.md5)) return
 
-    for (let field in obj) {
-      let rule = this.opts.fields[field]
-      let value = [obj[field]]
+    const _process = [key, val] => {
+      const rule = this.opts.fields[key]
+      let value = [val]
 
       if ('match' in rule) {
         value = value[0].match(rule.match)
@@ -57,50 +50,41 @@ class Feed {
           value = [rule.format(value)]
         } catch (err) {
           let string = rule.format
-          for (let i in value) {
-            string = string.replace(new RegExp('%' + i, 'g'), value[i])
-          }
+          value.map((val, i) => string = string.replace(new RegExp('%' + i, 'g'), val))
           value = [string]
         }
       }
 
-      item[field] = value[0]
+      item[key] = value[0]
     }
+    Object.entries(obj).map(_process)
     item.delay = item.updated - Date.now()
-    item.publish = item.delay <= 0 ? true : false
-
-    if (item.publish) this.cache.push(item.md5)
+    if (item.delay <= 0) this.cache.push(item.md5)
     this.item(item)
   }
 
-  _done () {
-    let limit = this.opts.feed.limit < 1
+  _done() {
+    const limit = this.opts.feed.limit < 1
       ? this.items.length
       : this.opts.feed.limit
 
-    let items = this.items.slice(0, limit)
-    if (this.opts.feed.reverse) {
-      items.reverse()
-    }
+    const items = this.items.slice(0, limit)
+    if (this.opts.feed.reverse) items.reverse()
 
-    let feed = new atom(this.opts.feed)
-    for (let item of items) {
-      if (item.publish) feed.entry(item)
-    }
+    const feed = new atom(this.opts.feed)
+    items.map(item => item.delay <= 0 && feed.entry(item))
 
-    let output =
-      { items: this.items
-      , atom: feed.toString()
-      }
+    const output =
+        { items: this.items
+        , atom: feed.toString()
+        }
     this.callback(output)
   }
 
-  item (obj) {
+  item(obj) {
     this.items.push(obj)
-    if (this.opts.ttl > 0) {
-      setTimeout(this.items.splice, this.opts.ttl
-        , this.items.indexOf(obj), 1).unref()
-    }
+    if (this.opts.ttl <= 0) return
+    setTimeout(this.items.splice, this.opts.ttl, this.items.indexOf(obj), 1)
   }
 
 }
@@ -115,44 +99,35 @@ class Manager {
     this.sched = later.parse.text(opts.fetch)
   }
 
-  data (func) {
-    this.cbs.push(func)
+  data(cb) {
+    this.cbs.push(cb)
     return this
   }
 
-  start () {
+  start() {
     later.date.localTime()
-    this.timer = later.setInterval(
-      this._fetch.bind(this)(), this.sched)
-    return this
+    this.timer = later.setInterval(this._fetch.bind(this)(), this.sched)
   }
 
-  stop () {
+  stop() {
     this.timer.clear()
     clearTimeout(this.forced)
   }
 
-  _fetch () {
+  _fetch() {
     if (this.logger) this.logger(Date())
-
-    let callback = obj => {
-      for (let cb of this.cbs) cb(obj)
-    }
+    const callback = obj => this.cbs.map(cb => cb(obj))
     this.feed.fetch(callback)
     return this._fetch.bind(this)
   }
 
-  _delay (feed) {
-    for (let item of feed.items) {
+  _delay(feed) {
+    const schedule = item => {
       if (this.logger) this.logger(item)
-      if (item.publish) continue
-
-      let next = later.schedule(this.sched)
-        .next(1, item.updated) - Date.now()
-
-      if (item.delay < next) this.forced = setTimeout(
-          this._fetch.bind(this), item.delay)
+      const next = later.schedule(this.sched).next(1, item.updated) - Date.now()
+      if (item.delay < next) this.forced = setTimeout(this._fetch.bind(this), item.delay)
     }
+    feed.items.filter(item => item.delay > 0).map(schedule)
   }
 
 }
--- public/example.js
+++ public/example.js
@@ -2,113 +2,113 @@ module.exports =
 
 // Later.js-parseable text schedule. See full syntax at
 // https://bunkat.github.io/later/parsers.html#text
-{ fetch: 'every day at 12:00 pm'
-, feed:
+  { fetch: 'every day at 12:00 pm'
+  , feed:
 
-   // Basic Atom feed information. Full options viewable at
-   // https://tools.ietf.org/html/rfc4287#section-4.1.1
-   //
-   // Required: title, id, author { name }, link_alternate
-   // Available: link_self, link_related, subtitle, contributor,
-   //            logo, icon, rights, generator
-  { title: 'Example Feed'
-  , subtitle: 'Example feed generated using Greedly'
-  , link_alternate: 'http://example.com'
-  , id: 'http://example.com'
-  , author: { name: 'John Doe' }
+     // Basic Atom feed information. Full options viewable at
+     // https://tools.ietf.org/html/rfc4287#section-4.1.1
+     //
+     // Required: title, id, author { name }, link_alternate
+     // Available: link_self, link_related, subtitle, contributor,
+     //            logo, icon, rights, generator
+    { title: 'Example Feed'
+    , subtitle: 'Example feed generated using Greedly'
+    , link_alternate: 'http://example.com'
+    , id: 'http://example.com'
+    , author: { name: 'John Doe' }
 
-  // (Optional) Limit the number of items that are fetched per update.
-  // Useful for only selecting most recent items in an ordered list.
-  // Remove or set to 0 for no limit.
-  , limit: 0
+    // (Optional) Limit the number of items that are fetched per update.
+    // Useful for only selecting most recent items in an ordered list.
+    // Remove or set to 0 for no limit.
+    , limit: 0
 
-  // (Optional) Reverse the order of items for output. Note that this
-  // won't change much if your feed aggregator uses timestamps. Reversal
-  // will occur AFTER applying the limit.
-  // Remove or set to false for no Reversal
-  , reverse: false
+    // (Optional) Reverse the order of items for output. Note that this
+    // won't change much if your feed aggregator uses timestamps. Reversal
+    // will occur AFTER applying the limit.
+    // Remove or set to false for no Reversal
+    , reverse: false
 
-  // (Optional) Time To Live for items in the feed, in ms. Previously fetched
-  // items will persist in the feed over various refreshes if feed.ttl > fetch.
-  // Remove or set to 0 for no item persistence.
-  , ttl: 24 * 60 * 60 * 1000
+    // (Optional) Time To Live for items in the feed, in ms. Previously fetched
+    // items will persist in the feed over various refreshes if feed.ttl > fetch.
+    // Remove or set to 0 for no item persistence.
+    , ttl: 24 * 60 * 60 * 1000
 
-  // (Optional) Request options for Needle. See
-  // https://github.com/tomas/needle#request-options
-  , request: { user_agent: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)' }
+    // (Optional) Request options for Needle. See
+    // https://github.com/tomas/needle#request-options
+    , request: { user_agent: 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)' }
 
-  // (Required) CSS/XPath selector for root feed item elements.
-  // See https://github.com/rchipka/node-osmosis/wiki/selectors.
-  , root: '.content .item'
+    // (Required) CSS/XPath selector for root feed item elements.
+    // See https://github.com/rchipka/node-osmosis/wiki/selectors.
+    , root: '.content .item'
+    }
+  , fields:
+     //
+     // The rule sets for extracting and formatting data for all entry fields
+     // are defined under the 'fields' key, using the same structure;
+     //
+     //   <field>: {
+     //     select: String                 (Required) CSS/XPath selector for element. Child of feed.root.
+     //                                    See https://github.com/rchipka/node-osmosis/wiki/selectors.
+     //
+     //   , [match: String|RegExp]         (Optional) String or RegExp to match the selected data against.
+     //                                    Any items that do not match will be discarded.
+     //
+     //   , [format: String|Function]      (Optional) Formatting for the selected data.
+     //                                    If a string is provided, string interpolation will occur using
+     //                                    field.format as a template and the result of field.select (or
+     //                                    field.format if present) for interpolation. Place holders are
+     //                                    denoted using the '%i' notation, with 0 = (original field.select),
+     //                                    and subsequent items corresponding to field.match'd groups.
+     //
+     //                                    E.g., prepending a base URL;
+     //                                      format: 'http://example.com/%0'
+     //
+     //                                    E.g., stripping '---' out of an item
+     //                                      match: /(.*)---(.*)/
+     //                                      format: '%1 %2'
+     //
+     //                                    If a function is provided, it must accept an array of strings
+     //                                    (with [0]  = origin field.select, and subsequent items corresponding
+     //                                    to field.match'd groups.) as sole argument and return a formatted string.
+     //
+     //                                    E.g., appending the date to a text entry
+     //                                      format: text => text[0] + (new Date).toString()
+     //   }
+     //
+     // Required fields: id, title, summary|content
+     // Available fields: link_alternate, link_enclosure, publish (must return Date object)
+     //   author, contributor, updated (defaults to Date.now, should return a valid Date object)
+     //
+     // Note: Greedly will not publish an item if the 'update' field is dated in
+     //   the future, and will instead delay publication until that time. This
+     //   feature can be used to generate 'notifications', for example for an
+     //   event. E.g.;
+     //
+     //     updated: {
+     //       select: 'span.event_date',
+     //       format: date => {
+     //         // publish the item 12 hours before the event date
+     //         let notify = new Date(date[0]) - 12 * 60 * 60 * 1000
+     //         return new Date(notify)
+     //       }
+     //     }
+     //
+    { id:
+      { select: 'a@href'
+      , format: 'http://example.com/%0'
+      }
+    , link_alternate:
+      { select: 'a@href'
+      , format: 'http://example.com/%0'
+      }
+    , title:
+      { select: 'h2.title'
+      , match: /(Article|Amazing)/
+      }
+    , summary: { select: 'p.description' }
+    , updated:
+      { select: 'span.date'
+      , format: date => new Date(date[0])
+      }
+    }
   }
-, fields:
-   //
-   // The rule sets for extracting and formatting data for all entry fields
-   // are defined under the 'fields' key, using the same structure;
-   //
-   //   <field>: {
-   //     select: String                 (Required) CSS/XPath selector for element. Child of feed.root.
-   //                                    See https://github.com/rchipka/node-osmosis/wiki/selectors.
-   //
-   //   , [match: String|RegExp]         (Optional) String or RegExp to match the selected data against.
-   //                                    Any items that do not match will be discarded.
-   //
-   //   , [format: String|Function]      (Optional) Formatting for the selected data.
-   //                                    If a string is provided, string interpolation will occur using
-   //                                    field.format as a template and the result of field.select (or
-   //                                    field.format if present) for interpolation. Place holders are
-   //                                    denoted using the '%i' notation, with 0 = (original field.select),
-   //                                    and subsequent items corresponding to field.match'd groups.
-   //
-   //                                    E.g., prepending a base URL;
-   //                                      format: 'http://example.com/%0'
-   //
-   //                                    E.g., stripping '---' out of an item
-   //                                      match: /(.*)---(.*)/
-   //                                      format: '%1 %2'
-   //
-   //                                    If a function is provided, it must accept an array of strings
-   //                                    (with [0]  = origin field.select, and subsequent items corresponding
-   //                                    to field.match'd groups.) as sole argument and return a formatted string.
-   //
-   //                                    E.g., appending the date to a text entry
-   //                                      format: text => text[0] + (new Date).toString()
-   //   }
-   //
-   // Required fields: id, title, summary|content
-   // Available fields: link_alternate, link_enclosure, publish (must return Date object)
-   //   author, contributor, updated (defaults to Date.now, should return a valid Date object)
-   //
-   // Note: Greedly will not publish an item if the 'update' field is dated in
-   //   the future, and will instead delay publication until that time. This
-   //   feature can be used to generate 'notifications', for example for an
-   //   event. E.g.;
-   //
-   //     updated: {
-   //       select: 'span.event_date',
-   //       format: date => {
-   //         // publish the item 12 hours before the event date
-   //         let notify = new Date(date[0]) - 12 * 60 * 60 * 1000
-   //         return new Date(notify)
-   //       }
-   //     }
-   //
-  { id:
-    { select: 'a@href'
-    , format: 'http://example.com/%0'
-    }
-  , link_alternate:
-    { select: 'a@href'
-    , format: 'http://example.com/%0'
-    }
-  , title:
-    { select: 'h2.title'
-    , match: /(Article|Amazing)/
-    }
-  , summary: { select: 'p.description' }
-  , updated:
-    { select: 'span.date'
-    , format: date => new Date(date[0])
-    }
-  }
-}
